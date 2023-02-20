348433f2577c25195d244ee0dbf738823d457fd2
Olivier Bussier <olivier@bussier.fr>
2023-02-21 00:29:11 +0100

    tentative

--- example/src/Pagination/index.jsx
+++ example/src/Pagination/index.jsx
@@ -1,5 +1,6 @@
+/* eslint-disable prettier/prettier */
 import React from 'react'
-import styles from "./style.scss";
+import './style.scss'
 
 const Button = ({
   id,
@@ -7,58 +8,58 @@ const Button = ({
   disabled = false,
   active,
   children,
-  className,
+  className
 }) => {
   return (
     <button
-      className={className + (active ? " active" : "")}
+      className={className + (active ? ' active' : '')}
       id={id}
       disabled={disabled}
       onClick={onClick}
     >
       {children}
     </button>
-  );
-};
+  )
+}
 
 export const Pagination = ({ nbItems, nbPerPage, curPage, onPageChange }) => {
-  let res = [];
-  const nbPages = Math.ceil(nbItems / nbPerPage);
+  const res = []
+  const nbPages = Math.ceil(nbItems / nbPerPage)
 
   res.push(
     <Button
-      className="left-btn"
-      key="btn-page-key-left"
+      className='left-btn'
+      key='btn-page-key-left'
       disabled={curPage === 0}
       onClick={() => onPageChange(curPage - 1)}
     >
-      {"<<"}
+      {'<<'}
     </Button>
-  );
+  )
 
   for (let i = 0; i < nbPages; i++) {
     res.push(
       <Button
-        className="middle-btn"
-        key={"btn-page-key-" + i}
+        className='middle-btn'
+        key={'btn-page-key-' + i}
         active={i === curPage}
         onClick={() => onPageChange(i)}
       >
         {i}
       </Button>
-    );
+    )
   }
 
   res.push(
     <Button
-      className="right-btn"
-      key="btn-page-key-right"
+      className='right-btn'
+      key='btn-page-key-right'
       disabled={nbPages === 0 || curPage === nbPages - 1}
       onClick={() => onPageChange(curPage + 1)}
     >
-      {">>"}
+      {'>>'}
     </Button>
-  );
+  )
 
-  return <div className="app-pagination">{res}</div>;
-};
+  return <div className='app-pagination'>{res}</div>
+}
